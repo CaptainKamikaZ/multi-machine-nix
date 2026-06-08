c2b94e73ee1395d07708b1c96c0a724e5bc7ba2e
Justin Gabrielson <justingabrielson@hotmail.com>
2026-06-08 06:21:55 -0500

    feat: remove greetd display manager support and related configuration

--- modules/features/default.nix
+++ modules/features/default.nix
@@ -5,7 +5,6 @@
     ./obs.nix
     ./audiotools.nix
     ./gaming.nix
-    ./greetd.nix
     ./sddm.nix
     ./virtualization.nix
   ];
@@ -14,7 +13,6 @@
     obs.enable = lib.mkEnableOption "OBS Studio and plugins";
     audiotools.enable = lib.mkEnableOption "Audio tools";
     gaming.enable = lib.mkEnableOption "Gaming applications";
-    greetd.enable = lib.mkEnableOption "Greetd display manager";
     sddm.enable = lib.mkEnableOption "SDDM display manager";
     virtualization.enable = lib.mkEnableOption "Virtualization tools";
   };

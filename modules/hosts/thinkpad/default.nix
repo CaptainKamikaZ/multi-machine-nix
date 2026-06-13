172f89e80b75f005c33446ec6c0435789f875926
Justin Gabrielson <justingabrielson@hotmail.com>
2026-06-13 14:10:17 -0500

    feat: clean up default.nix by removing unused virtualization feature and commented-out options

--- modules/hosts/thinkpad/default.nix
+++ modules/hosts/thinkpad/default.nix
@@ -11,33 +11,21 @@
     ./services.nix
 
     # Shared defaults (flake‑relative)
+    "${inputs.self}/modules/shared/system.nix"
     "${inputs.self}/modules/shared/nix.nix"
     "${inputs.self}/modules/shared/fonts.nix"
     "${inputs.self}/modules/shared/packages.nix"
     "${inputs.self}/modules/shared/audio.nix"
     "${inputs.self}/modules/shared/printing.nix"
     "${inputs.self}/modules/shared/users.nix"
-    "${inputs.self}/modules/shared/virtualization.nix"
     "${inputs.self}/modules/shared/thunar.nix"
     "${inputs.self}/modules/shared/grub-theme.nix"
 
     # Features
     "${inputs.self}/modules/features/default.nix"
-    # Add these when ready:
-    # "${inputs.self}/modules/features/niri"
-    # "${inputs.self}/modules/features/noctalia"
   ];
 
-  # Enable features for this host
   features.sddm.enable = true;
-
-  # Enable these when ready:
-  # features.niri.enable = true;
-  # features.noctalia.enable = true;
-
-  environment.systemPackages = with pkgs; [
-    kdePackages.kirigami
-  ];
 
   system.stateVersion = "25.11";
 }

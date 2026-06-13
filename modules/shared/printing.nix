0ad0dc2dfbee3c676cf028e1e830d947b5eb15e7
Justin Gabrielson <justingabrielson@hotmail.com>
2026-06-13 14:19:36 -0500

    feat: add printer configuration for hpprinter with network settings

--- modules/shared/printing.nix
+++ modules/shared/printing.nix
@@ -1,3 +1,17 @@
 {
   services.printing.enable = true;
+  hardware.printers.ensurePrinters = [
+    {
+      name = "hpprinter";
+      deviceUri = "ipp://192.168.0.80/ipp/print";
+      model = "everywhere";
+    }
+  ];
+  services.printing.defaultShared = false;
+  services.printing.listenAddresses = [ "localhost" ];
+  services.printing.allowFrom = [ "localhost" ];
+  systemd.services.ensure-printers = {
+    after = [ "network-online.target" "cups.service" ];
+    wants = [ "network-online.target" ];
+  };
 }

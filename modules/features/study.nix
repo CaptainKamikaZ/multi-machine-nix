6135cd18dcd072b821e900ac30ce57db60b8941b
Justin Gabrielson <justingabrielson@hotmail.com>
2026-06-23 06:17:31 -0500

    refined Anki wrapper, added mpv

--- modules/features/study.nix
+++ modules/features/study.nix
@@ -6,13 +6,19 @@ in
 {
   config = lib.mkIf cfg.enable {
 
-    # Only install the wrapper, not anki-bin itself
-    home-manager.users.justin.home.packages = [
-      (pkgs.writeShellScriptBin "anki" ''
-        export ANKI_WAYLAND=1
-        exec ${pkgs.anki-bin}/bin/anki "$@"
-      '')
+    home-manager.users.justin.home.packages = lib.mkMerge [
+      [
+        (pkgs.writeShellScriptBin "anki" ''
+          export ANKI_WAYLAND=1
+          export QT_QPA_PLATFORM=xcb
+          export QTWEBENGINE_CHROMIUM_FLAGS="--disable-gpu --disable-software-rasterizer"
+          exec ${pkgs.anki-bin}/bin/anki "$@"
+        '')      
+      ]
+      [ pkgs.mpv ]
+      [ pkgs.adwaita-qt ]
     ];
+
 
     # Override .desktop entry so launchers use Wayland
     home-manager.users.justin.xdg.desktopEntries.anki = {

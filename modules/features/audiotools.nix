9fcab11abbaa0fd3443dbfadab962abb67fc855a
Justin Gabrielson <justingabrielson@hotmail.com>
2026-06-27 16:40:10 -0500

    configured easyeffects

--- modules/features/audiotools.nix
+++ modules/features/audiotools.nix
@@ -2,9 +2,25 @@
 
 {
   config = lib.mkIf config.features.audiotools.enable {
-    home-manager.users.justin.home.packages = with pkgs; [
-      easyeffects
-      helvum
-    ];
+
+    home-manager.users.justin = {
+      services.easyeffects.enable = true;
+
+      systemd.user.services.easyeffects = {
+
+        Service = {
+          # Automatically clean up the broken socket files if they exist before launching
+          ExecStartPre = [
+            "-/bin/sh -c 'rm -f /tmp/easyeffects.lock /tmp/EasyEffectsServer \${XDG_RUNTIME_DIR}/easyeffects.lock \${XDG_RUNTIME_DIR}/EasyEffectsServer'"
+          ];
+          ExecStart = lib.mkForce "${pkgs.easyeffects}/bin/easyeffects --hide-window --service-mode";
+        };
+      };
+
+      home.packages = with pkgs; [
+        easyeffects
+        helvum
+      ];
+    };
   };
 }

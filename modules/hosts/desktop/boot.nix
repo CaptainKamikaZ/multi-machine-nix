7c178054e5c67f9188af456a1f880fcd329254d1
Justin Gabrielson <justingabrielson@hotmail.com>
2026-09-13 22:24:22 -0500

    Add extra boot entry for Omarchy Linux in boot.nix

--- modules/hosts/desktop/boot.nix
+++ modules/hosts/desktop/boot.nix
@@ -12,5 +12,14 @@
     device = "nodev";
     configurationLimit = 10;
     useOSProber = true;
+    extraEntries = ''
+      menuentry "Omarchy Linux" {
+        insmod part_gpt
+        insmod fat
+        insmod chain
+        search --no-floppy --fs-uuid --set=root E3FD-1014
+        chainloader /EFI/BOOT/BOOTX64.EFI
+      }
+    '';
   };
 }
